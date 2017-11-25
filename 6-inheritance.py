0705020fcabdc35aa9ac7c788b4484f9094e43fb
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-25 18:57:53 -0500

    add 6-inheritance.py

---
+++ 6-inheritance.py
@@ -0,0 +1,53 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    def friend(self, friend_name):
+        return Student(friend_name, self.school)
+
+anna = Student("Anna", "Oxford")
+
+friend = anna.friend("Greg")
+print(friend.name)
+print(friend.school)
+
+###
+# inherit student
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+rolf = WorkingStudent("Rolf", "Harvard", 20.00)
+sue = rolf.friend("Sue")
+#print(sue.salary)  # Error sue is just a student object. FIX BELOW:
+
+
+### ================================
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    @classmethod
+    def friend(cls, origin, friend_name, *args):
+        return cls(friend_name, origin.school, *args)
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+rolf = WorkingStudent("Rolf", "Harvard", 20.00)
+sue = WorkingStudent.friend(rolf, "Sue", 15.00)
+print(sue.salary)  #  works
